3af87f42db04f56ec9901bcd566c7e440ac7b543
andy3659 <andy3659@gmail.com>
2019-12-01 18:54:04 +0700

    slide tebak(Ade)

--- dist/score/score.js
+++ dist/score/score.js
@@ -1,5 +1,7 @@
 var scoreA = 0;
 var scoreB = 0;
+var tebakA= 0;
+var tebakB =0;
 //var harga1 = 12900;
 //var harga2 = 50000;
 
@@ -24,14 +26,14 @@ var slider_minuman_A = document.getElementById("minuman_slider_A");
 var nilai_slider_minuman_A = document.getElementById("nilai_sliderMinuman_A");
 
 slider_minuman_A.oninput = function(){
-    nilai_slider_minuman_A.innerHTML = minumA.value;
+    nilai_slider_minuman_A.innerHTML = slider_minuman_A.value;
 }
 
 var slider_minuman_B = document.getElementById("minuman_slider_B");
 var nilai_slider_minuman_B = document.getElementById("nilai_sliderMinuman_B");
 
 slider_minuman_B.oninput = function(){
-    nilai_slider_minuman_B.innerHTML = minumB.value;
+    nilai_slider_minuman_B.innerHTML = slider_minuman_B.value;
 }
 
 
@@ -44,10 +46,9 @@ function submit(kode_hitungan, harga_item, tebakan_A, tebakan_B, nama_button, na
 
     scoreA+=selisihA
     scoreB+=selisihB
-
-    document.getElementById("nama_text_harga").innerHTML = "Real Price : " + harga_item;
-    document.getElementById("nama_text_harga").style.visibility = "visible";
-    document.getElementById("nama_button").style.visibility = "hidden";
+    document.getElementById(nama_text_harga).innerHTML = "Real Price : " + harga_item;
+    document.getElementById(nama_text_harga).style.visibility = "visible";
+    document.getElementById(nama_button).style.visibility = "hidden";
 /*
     if (selisihA < selisihB) {
       scoreA += 1;
@@ -70,5 +71,45 @@ function submit(kode_hitungan, harga_item, tebakan_A, tebakan_B, nama_button, na
     document.getElementById("progress1A").style.visibility = "visible";
     document.getElementById("progress1B").style.visibility = "visible";
 */ 
+  }
+}
+function submit(harga,perkiraan){
+  if(tebakA==0 && perkiraan<harga){
+    scoreA+=1
+  }
+  else if(tebakA==0 && perkiraan>harga){
+    scoreA+=1
+  }
+  if(tebakB==0 && perkiraan<harga){
+    scoreB+=1
+  }
+  else if(tebakB==0 && perkiraan>harga){
+    scoreB+=1
+  }
+  console.log(scoreA);
+  console.log(scoreB);
+}
+
+
+
+
+function tebak(team,coba) {
+  if(team=="A")
+  {
+    if(coba == "rendah"){
+      tebakA=0
+    }
+    else{
+      tebakA=1
+    }
+  }
+  else
+  {
+    if(coba == "rendah"){
+      tebakB=0
+    }
+    else{
+      tebakB=1
+    }
   }
 }
